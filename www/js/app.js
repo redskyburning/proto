c3592624a4a81deef1cb47b9c6f6b984bf5ce16c
Ben Webb <ben@redskyburning.com>
2013-02-20 11:47:23 -0500

    decreasing pages

--- www/js/app.js
+++ www/js/app.js
@@ -72,7 +72,7 @@ function prepRem() {
 
 $(document).ready(function(){
 		
-		var numPages = 5;
+		var numPages = 3;
 		for(i=0; i < numPages; i++){
 			$('#app-stack').append($('#test-page').html());
 		}
